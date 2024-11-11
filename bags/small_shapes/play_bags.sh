5fe6192ed801aa9970c04871dcbd094546b33e5e
wbc_dssl_desktop <weibingchuan20@gmail.com>
2024-11-11 22:09:27 +0800

    Add bag play sh

---
+++ bags/small_shapes/play_bags.sh
@@ -0,0 +1,5 @@
+gnome-terminal -- zsh -c "ros2 bag play robot0/robot0_vel/robot0_vel_0.db3" 
+
+gnome-terminal -- zsh -c "ros2 bag play robot1/robot1_vel/robot1_vel_0.db3" 
+
+gnome-terminal -- zsh -c "ros2 bag play robot2/robot2_vel/robot2_vel_0.db3" 
